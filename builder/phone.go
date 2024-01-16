63fec39349d85024442ea3c1d8e013ce02e22e6d
zuki <zuki@home>
2024-01-16 23:14:21 +0630

    code clean up

--- builder/phone.go
+++ builder/phone.go
@@ -12,12 +12,6 @@ func (phone *Phone) GetInstance(brand_name string, price string, quantity int) {
 	phone.Price = price
 	phone.Quantity = quantity
 	phone.ProductType = "Phone"
-	// return Product{
-	// 	BrandName:   brand_name,
-	// 	Price:       price,
-	// 	ProductType: "Phone",
-	// 	Quantity:    quantity,
-	// }
 }
 
 func (phone *Phone) GetProductInfo() Product {
